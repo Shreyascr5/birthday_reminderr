ce3d9d04d8f491521288cf26fe8c6d245d579b7e
Shreyascr5 <shreyas050801@gmail.com>
2023-08-11 12:04:28 +0530

    completed proj with styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import data from "./data";
+import List from "./List";
 
 function App() {
+  const [people,setPeople]=useState(data);
   return (
-    <div className="App">
-    
-    </div>
+   <main>
+    <section className="container">
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}/>
+      <button onClick={()=>{
+       setPeople([]);
+      }}>Clear all</button>
+    </section>
+   </main>
   );
 }
 
